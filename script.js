35aa821b317f76fa6f65d797b6e9e9f30b19b556
sakshyam gautam <sakshyamgautam79@gmail.com>
2026-05-31 07:52:27 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const question = [
+    { 
+        question: "Which animal is the largest in the world",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+
+    },
+    {
+       question: "Which is the smallest continent in the world",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "North America", correct: false },
+            { text: "Africa", correct: false },
+        ]  
+    },
+    {
+        question: "Which is the largest desert in the world",
+        answers: [
+            { text: "Kalakhari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Antarctic Desert", correct: true },
+        ]  
+    },
+    {
+        question: "Which is the smallest country in the world",
+        answers: [
+            { text: "Vatican City", correct: true },
+            { text: "Nepal", correct: false },
+            { text: "Russia", correct: false},
+            { text: "Maldives", correct: false },
+        ]  
+    },
+    {
+        question: "Where is Mount Everest located",
+        answers: [
+            { text: "China", correct: false },
+            { text: "India", correct: false },
+            { text: "Nepal", correct: true},
+            { text: "Bhutan", correct: false },
+        ]  
+    }
+];
+
+const  questionElement = document.getElementById("question");
+const questionElement = document.getElementById("");
